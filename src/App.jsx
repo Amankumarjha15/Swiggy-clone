a1bb3da1f3950fce6cf89dcab0a4da018f39a69a
Amankumarjha15 <amanjhakr6299@gmail.com>
2024-09-16 17:39:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Head from "./Components/Head"
+
+function App() {
+
+  return (
+   <div>
+    <Head/>
+   </div>
+  )
+}
+
+export default App
---
+++ src/Components/Head.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Head() {
+  return (
+    <div className='w-full shadow-md h-24 flex justify-center items-center'>
+        
+
+        <div className='border border-black w-[70%]'>
+        <img className='w-24' src="https://1000logos.net/wp-content/uploads/2021/05/Swiggy-emblem.png" alt="" />
+        </div>
+
+
+
+
+    </div>
+  )
+}
+
+export default Head
