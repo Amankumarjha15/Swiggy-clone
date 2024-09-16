98a402a9ce43542b0cf4413966371853a6aa38e2
Amankumarjha15 <amanjhakr6299@gmail.com>
2024-09-16 21:23:12 +0530

    navbar half updated

--- src/Components/Head.jsx
+++ src/Components/Head.jsx
@@ -5,9 +5,59 @@ function Head() {
     <div className='w-full shadow-md h-24 flex justify-center items-center'>
         
 
-        <div className='border border-black w-[70%]'>
-        <img className='w-24' src="https://1000logos.net/wp-content/uploads/2021/05/Swiggy-emblem.png" alt="" />
-        </div>
+  <div className='w-[70%] flex justify-between'>
+    <div className='flex items-center'>
+
+              <img className='w-24' src="https://1000logos.net/wp-content/uploads/2021/05/Swiggy-emblem.png" alt="" />
+
+       <div className='flex items-center gap-1'>
+
+            <p className='font-bold border-b-2 border-black'>others</p>
+            <i class="fi text-orange-500 text-2xl mt-2 fi-rs-angle-small-down"></i>
+
+       </div>
+    </div>
+           <div className='flex items-center gap-6'>
+            <div className='flex'>
+              <i class="fi fi-rr-shopping-bag"></i>
+              <p>Swiggy Corporate</p>
+            </div>
+           
+            <div className='flex'>
+              <i class="fi fi-rr-shopping-bag"></i>
+              <p>Search</p>
+            </div>
+           
+            <div className='flex'>
+              <i class="fi fi-rr-shopping-bag"></i>
+              <p>Offers</p>
+            </div>
+            
+            <div className='flex'>
+              <i class="fi fi-rr-shopping-bag"></i>
+              <p>Help</p>
+            </div>
+           
+            <div className='flex'>
+              <i class="fi fi-rr-shopping-bag"></i>
+              <p>Sign in</p>
+            </div>
+
+            <div className='flex'>
+              <i class="fi fi-rr-shopping-bag"></i>
+              <p>Cart</p>
+            </div>
+
+            <div className='flex'>
+              <i class="fi fi-rr-shopping-bag"></i>
+              <p>Swiggy Corporate</p>
+            </div>
+           
+           </div>
+         
+
+
+   </div>
 
 
 
