e70ecec5438fe5633e415410279fd60e4539ba5c
Amankumarjha15 <amanjhakr6299@gmail.com>
2024-09-29 14:30:00 +0530

    redux store is set-up

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {BrowserRouter} from 'react-router-dom'
+import store from './utils/store.js'
+import {Provider} from "react-redux"
 
 createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
   <BrowserRouter>
   <StrictMode>
     <App />
   </StrictMode>
   </BrowserRouter>
+  </Provider>
 )
