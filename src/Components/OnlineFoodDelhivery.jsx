721796968ed64aed6423f6d2a80d129c4acf602e
Amankumarjha15 <amanjhakr6299@gmail.com>
2024-10-01 23:03:22 +0530

    filter buttons are cretaed

--- src/Components/OnlineFoodDelhivery.jsx
+++ src/Components/OnlineFoodDelhivery.jsx
@@ -3,11 +3,40 @@ import ResturantCard from "./ResturantCard";
 
 function OnlineFoodDelhivery({ Data , title}) {
   // console.log(Data)
+ const filterOptions =[
+    {
+      Name : "Rating 4.0+"
+    },
+    {
+      Name : "Less Than Rs . 300"
+    },
+    {
+      Name : "Rs . 300 - Rs . 600"
+    },
+    {
+      Name : "Offers"
+    },
+  ]
   return (
     <>
       <div>
         <div className="my-8">
         <p className='font-bold text-2xl'>{title}</p>
+              <div className="my-7 flex gap-3">
+                {
+                  filterOptions &&
+                  filterOptions.map((data)=>(
+                    <button onClick={} className='filterBtn flex gap-3'>
+                       <p>{data.Name}</p>
+                       <i className="fi text-sm mt-1 fi-br-cross-small hidden"></i>
+                       </button>
+                  ))
+                }
+                 {/* <button className='filterBtn'>Rating 4.0+</button>
+                  <button className='filterBtn'>Offers</button>
+                  <button className='filterBtn'>Rs . 300 - Rs . 600 </button>
+                  <button className='filterBtn'>Less Than Rs . 300</button> */}
+              </div>
         </div>
         <div className="grid grid-cols-4 gap-3">
         {
