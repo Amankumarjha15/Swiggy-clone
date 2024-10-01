9792adaaba956b79805643b8a1d3fa0d64b63675
Amankumarjha15 <amanjhakr6299@gmail.com>
2024-10-01 19:27:04 +0530

    react hot toast were created inn initialized

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,14 @@ import './index.css'
 import {BrowserRouter} from 'react-router-dom'
 import store from './utils/store.js'
 import {Provider} from "react-redux"
+import {Toaster} from "react-hot-toast"
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
   <BrowserRouter>
   <StrictMode>
     <App />
+    <Toaster/>
   </StrictMode>
   </BrowserRouter>
   </Provider>
