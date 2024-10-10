4b94d2182be5da7863fb160575b17f6c19deeed8
Amankumarjha15 <amanjhakr6299@gmail.com>
2024-10-10 14:33:18 +0530

    custom hooks are created

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -6,67 +6,71 @@ import OnlineFoodDelhivery from './OnlineFoodDelhivery';
 import { Coordinates } from '../context/contextApi';
 import { useSelector } from "react-redux";
 import Shimmer from './Shimmer';
+import useRestaurantData from '../../Hooks/useRestaurantData';
 
 
 function Body() {
 
-  const [onYourMind, setonYourMind] = useState([])
-  const [topResturant, setTopResturant] = useState([])
 
-  const [ResultData, setResultData] = useState({})
+  const  [onYourMind,topResturant,ResultData,TopResTitle,OnlineTitle] = useRestaurantData()
 
-  const [TopResTitle, setTopResTitle] = useState("")
-  const [OnlineTitle, setOnlineTitle] = useState("")
+//   const [onYourMind, setonYourMind] = useState([])
+//   const [topResturant, setTopResturant] = useState([])
+
+//   const [ResultData, setResultData] = useState({})
+
+//   const [TopResTitle, setTopResTitle] = useState("")
+//   const [OnlineTitle, setOnlineTitle] = useState("")
 
 
 
-  const {Coord : {lat , lng}} = useContext(Coordinates);
+//   const {Coord : {lat , lng}} = useContext(Coordinates);
 
   
-  async function fetchData(){
-    const Data = await fetch(`https://www.swiggy.com/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`)
-    const result = await Data.json();
+//   async function fetchData(){
+//     const Data = await fetch(`https://www.swiggy.com/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`)
+//     const result = await Data.json();
 
 
-    // console.log(result)
+//     // console.log(result)
 
-    setResultData(result?.data?.cards[0]?.card?.card?.title)
+//     setResultData(result?.data?.cards[0]?.card?.card?.title)
 
-    let mainData = result?.data?.cards.find(
-      (data) => data?.card?.card?.id == "top_brands_for_you"
-  )?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+//     let mainData = result?.data?.cards.find(
+//       (data) => data?.card?.card?.id == "top_brands_for_you"
+//   )?.card?.card?.gridElements?.infoWithStyle?.restaurants;
 
-    let mainData2 = result?.data?.cards.find(
-      (data) => data?.card?.card?.id == "restaurant_grid_listing"
-  )?.card?.card?.gridElements?.infoWithStyle?.restaurants;
-
-
-  //   let onminddata = result?.data?.cards.find(
-  //     (data) => data?.card?.card?.id == "whats_on_your_mind"
-  // )?.card?.card?.imageGridCards?.info;
+//     let mainData2 = result?.data?.cards.find(
+//       (data) => data?.card?.card?.id == "restaurant_grid_listing"
+//   )?.card?.card?.gridElements?.infoWithStyle?.restaurants;
 
 
-  let onminddata = result?.data?.cards.find(
-    (data) => data?.card?.card?.id == "whats_on_your_mind"
-)?.card?.card?.imageGridCards?.info;
+//   //   let onminddata = result?.data?.cards.find(
+//   //     (data) => data?.card?.card?.id == "whats_on_your_mind"
+//   // )?.card?.card?.imageGridCards?.info;
 
-// console.log(data2)
 
-    setTopResturant(mainData || mainData2);
+//   let onminddata = result?.data?.cards.find(
+//     (data) => data?.card?.card?.id == "whats_on_your_mind"
+// )?.card?.card?.imageGridCards?.info;
 
-    // setTopResturant(result?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+// // console.log(data2)
+
+//     setTopResturant(mainData || mainData2);
+
+//     // setTopResturant(result?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
  
-    // setonYourMind(result?.data?.cards[0]?.card?.card?.imageGridCards?.info);
+//     // setonYourMind(result?.data?.cards[0]?.card?.card?.imageGridCards?.info);
     
-    setonYourMind(onminddata)
+//     setonYourMind(onminddata)
 
 
-    setOnlineTitle(result?.data?.cards[2]?.card?.card?.title);
-    setTopResTitle(result?.data?.cards[1]?.card?.card?.header?.title);
-  }
-  useEffect(()=>{
-    fetchData()
-  },[lat , lng])
+//     setOnlineTitle(result?.data?.cards[2]?.card?.card?.title);
+//     setTopResTitle(result?.data?.cards[1]?.card?.card?.header?.title);
+//   }
+//   useEffect(()=>{
+//     fetchData()
+//   },[lat , lng])
   
 
 
