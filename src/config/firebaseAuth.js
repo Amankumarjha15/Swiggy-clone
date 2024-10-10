8b3db403b3a7f11d4b3e729866358f1bde5458d3
Amankumarjha15 <amanjhakr6299@gmail.com>
2024-10-10 18:32:21 +0530

    env edit

--- src/config/firebaseAuth.js
+++ src/config/firebaseAuth.js
@@ -2,17 +2,17 @@
 import { initializeApp } from "firebase/app";
 
 
-import { getAuth, GoogleAuthProvider } from "firebase/auth";``
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_PROJECTID,
-  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGE,
-  appId: import.meta.env.VITE_APPID,
+  "apiKey": import.meta.env.VITE_APIKEY,
+  "authDomain": import.meta.env.VITE_AUTHDOMAIN,
+  "projectId": import.meta.env.VITE_PROJECTID,
+  "storageBucket": import.meta.env.VITE_STORAGEBUCKET,
+  "messagingSenderId": import.meta.env.VITE_MESSAGE,
+  "appId": import.meta.env.VITE_APPID,
 }
 
 
