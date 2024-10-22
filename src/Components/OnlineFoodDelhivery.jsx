d93ff89d844b31694f1cc34fb47db4cc0df57219
Amankumarjha15 <amanjhakr6299@gmail.com>
2024-10-22 16:00:03 +0530

    more responsiveness added in design

--- src/Components/OnlineFoodDelhivery.jsx
+++ src/Components/OnlineFoodDelhivery.jsx
@@ -42,7 +42,7 @@ dispatch(setFilterValue(activeBtn))
                   <button className='filterBtn'>Less Than Rs . 300</button> */}
               </div>
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-3">
         {
         Data &&
         Data.map((restaurant,i) => (
